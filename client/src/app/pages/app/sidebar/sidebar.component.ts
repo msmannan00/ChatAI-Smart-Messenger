8f1f0d48aa1c0a1bd67853e2a2f92f14a30daf54
Abdul Mannan Saeed <msmannan00@protonmail.com>
2023-12-08 22:14:36 +0500

    side bar features added

--- client/src/app/pages/app/sidebar/sidebar.component.ts
+++ client/src/app/pages/app/sidebar/sidebar.component.ts
@@ -1,9 +1,13 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule
+  ],
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
@@ -11,6 +15,5 @@ export class SidebarComponent {
   @Input() isExpanded: boolean = false;
   @Output() toggleSidebar: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-  handleSidebarToggle = () => this.toggleSidebar.emit(!this.isExpanded);
-
+  userMood: string = 'option1';
 }
